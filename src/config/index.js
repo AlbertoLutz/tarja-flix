513107e0f055510a86d018f626ad3dfddbfd40fc
Alberto Lutz <albertolutzdias@gmail.com>
2021-05-08 00:19:29 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,4 @@ const URL_BASE = window.location.hostname.includes("localhost")
 	? "http://localhost:8080"
 	: "https://tarja-flix.herokuapp.com/categorias";
 
-export default {URL_BASE};
+export default URL_BASE;
